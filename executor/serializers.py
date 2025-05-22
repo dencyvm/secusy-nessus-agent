2d0a56240b31a7a5dd309af220e7fe6022c92ef3
Asifali <asifali.ek@valuementor.com>
2025-05-22 10:03:42 +0530

    serializer updation

--- executor/serializers.py
+++ executor/serializers.py
@@ -4,4 +4,4 @@ from executor.models import Scan
 class ScanSerializer(serializers.ModelSerializer):
     class Meta:
         model = Scan
-        fields = ['scan_id', 'created_at', 'target', 'scan_status', 'result_url','errors','resolved_target','policy_type','policy_file','scan_scope', 'org_ref_id']
+        fields = ['scan_id', 'created_at', 'target', 'scan_status', 'result_url','errors','resolved_target','policy_type','policy_file','scan_scope', 'org_ref_id', 'scan_type']
